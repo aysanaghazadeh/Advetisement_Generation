cf333d7aeb060fb031993ea93452b3b66488ab93
AysanAghazadeh <aya34@pitt.edu>
2024-04-01 17:11:46 -0400

    config updated

---
+++ configs/config.py
@@ -0,0 +1,95 @@
+import torch
+import yaml
+import sys
+import argparse
+
+
+def read_yaml_config(file_path):
+    """Reads a YAML configuration file and returns a dictionary of the settings."""
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+
+def convert_to_args(config):
+    """Converts a nested dictionary to a list of command-line arguments."""
+
+    args = {}
+    for section, settings in config.items():
+        for key, value in settings.items():
+            args[key] = value
+    return args
+
+
+def set_conf(config_file):
+    yaml_file_path = config_file
+    config = read_yaml_config(yaml_file_path)
+    args = convert_to_args(config)
+    return args
+
+
+def parse_args():
+    """ Parsing the Arguments for the Advertisement Generation Project"""
+    parser = argparse.ArgumentParser(description="Configuration arguments for advertisement generation:")
+    parser.add_argument('--config_type',
+                        type=str,
+                        required=True,
+                        help='Choose among ARGS for commandline arguments, DEFAULT for default values, or YAML for '
+                             'config file')
+    parser.add_argument('--config_path',
+                        type=str,
+                        default=None,
+                        help='The path to the config file if config_type is YAML')
+    parser.add_argument('--T2I-model',
+                        type=str,
+                        default='PixArt',
+                        help='T2I generation model chosen from: PixArt, Expressive, ECLIPSE, Translate')
+    parser.add_argument('--LLM',
+                        type=str,
+                        default='Mixtral7B',
+                        help='LLM chosen from: Mixtral7B, Mistral7B, Vicuna, LLaMA2')
+    parser.add_argument('--train',
+                        type=bool,
+                        default=False,
+                        help='True if the LLM is being fine-tuned')
+    parser.add_argument('--data_path',
+                        type='str',
+                        default='../Data/PittAd',
+                        help='Path to the root of the data'
+                        )
+    parser.add_argument('--train-setQA',
+                        type=str,
+                        default=None,
+                        help='If the model is fine-tuned, relative path to the train-set QA from root path')
+    parser.add_argument('--train-setImages',
+                        type=str,
+                        default=None,
+                        help='If the model is fine-tuned, relative path to the train-set Images from root path')
+    parser.add_argument('--test-setQA',
+                        type=str,
+                        default='train/large_combined_hard_QA_Combined_Action_Reason_train.json',
+                        help='Relative path to the QA file for action-reasons from root path'
+                        )
+    parser.add_argument('--test-setImages',
+                        type=str,
+                        default='train_images',
+                        help='Relative path to the original images for the test set from root')
+    parser.add_argument('--text-input-type',
+                        type=str,
+                        default='AR',
+                        help='Type of the input text for T2I generation model. Choose from LLM, AR (for action-reason),'
+                             ' combine, IN, VT, atypicality')
+    parser.add_argument('--description_file',
+                        type=str,
+                        default=None,
+                        help='Path to the description file for the T2I input.')
+    return parser.parse_args()
+
+args = parse_args()
+if args.config_type == 'YAML':
+    args = set_conf(args.config_path)
+args['device'] = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("Arguments are:\n", '\n'.join(args), '\n', '-'*40)
+
+
+
+

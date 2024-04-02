65c1affeb8304c551e6bb294cd549566680412a9
AysanAghazadeh <aya34@pitt.edu>
2024-04-01 23:09:01 -0400

    April 1 last update

--- T2I_models/T2I_model.py
+++ T2I_models/T2I_model.py
@@ -2,7 +2,7 @@ from torch import nn
 from PitxArt import PixArt
 
 
-class T2I_model(nn.Module):
+class T2IModel(nn.Module):
     def __init__(self, args):
         model_map = {
             'PixArt': PixArt
@@ -10,4 +10,4 @@ class T2I_model(nn.Module):
         self.model = model_map[args.T2I_model](args)
 
     def forward(self, prompt):
-        self.model(prompt)
+        return self.model(prompt)
---
+++ advertisement_generation/pipeline.py
@@ -0,0 +1,15 @@
+from T2I_models.T2I_model import T2IModel
+from utils.prompt_engineering.prompt_generation import PromptGenerator
+from torch import nn
+
+
+class AdvertisementImageGeneration(nn.Module):
+    def __init__(self, args):
+        self.args = args
+        self.prompt_generator = PromptGenerator(self.args)
+        self.T2I_model = T2IModel(args)
+
+    def forward(self, image_filename):
+        prompt = self.prompt_generator(self.args, image_filename)
+        image = self.T2I_model(prompt)
+        return image

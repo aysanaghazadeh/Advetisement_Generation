a64db5e5fe245259dcecc4d99a9de9b80cc8ed35
AysanAghazadeh <aya34@pitt.edu>
2024-04-02 11:13:01 -0400

    metrics added

---
+++ Evaluation/metrics.py
@@ -0,0 +1,49 @@
+from pytorch_fid.fid_score import calculate_fid_given_paths
+import clip
+from PIL import Image
+import torch
+
+
+def get_FID(generated_image_path, real_image_path, args):
+    fid_value = calculate_fid_given_paths([[real_image_path], [generated_image_path]],
+                                          device=args.device)
+    return fid_value
+
+
+def get_image_image_CLIP_score(generated_image_path, real_image_path, args):
+    model, preprocess = clip.load("ViT-B/32", device=args.device)
+    # Preprocess the images
+    image1 = preprocess(Image.open(generated_image_path)).unsqueeze(0).to(args.device)
+    image2 = preprocess(Image.open(real_image_path)).unsqueeze(0).to(args.device)
+
+    # Compute the features
+    with torch.no_grad():
+        image_features1 = model.encode_image(image1)
+        image_features2 = model.encode_image(image2)
+
+    # Normalize the features
+    image_features1 = image_features1 / image_features1.norm(dim=-1, keepdim=True)
+    image_features2 = image_features2 / image_features2.norm(dim=-1, keepdim=True)
+
+    # Compute the cosine similarity
+    cosine_similarity = (image_features1 @ image_features2.T).item()
+
+    return cosine_similarity
+
+
+def get_text_image_CLIP_score(generated_image_path, text_description, args):
+    model, preprocess = clip.load("ViT-B/32", device=args.device)
+    image = preprocess(Image.open(generated_image_path)).unsqueeze(0).to(args.device)
+    text_tokens = clip.tokenize([text_description]).to(args.device)
+
+    # Compute the image and text features
+    with torch.no_grad():
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(text_tokens)
+
+    # Compute the similarity score (cosine similarity)
+    similarity_score = (image_features @ text_features.T).softmax(dim=-1)
+
+    return similarity_score
+
+

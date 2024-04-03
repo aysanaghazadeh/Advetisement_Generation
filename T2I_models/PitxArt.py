9a7db61da3f8546c44f3968e7827a2c30d0c4820
AysanAghazadeh <aya34@pitt.edu>
2024-04-03 05:15:45 -0400

    import error fixed

--- T2I_models/PitxArt.py
+++ T2I_models/PitxArt.py
@@ -7,7 +7,7 @@ class PixArt(nn.Module):
     def __init__(self, args):
         super(PixArt, self).__init__()
         self.pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", torch_dtype=torch.float16)
-        self.pipe = self.pipe.to(device=args.device)
+        # self.pipe = self.pipe.to(device=args.device)
 
     def forward(self, prompt):
         image = self.pipe(prompt).images[0]

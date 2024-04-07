b3cf260ce54311447a1f496f8e0d9a8e120b6ab0
AysanAghazadeh <aya34@pitt.edu>
2024-04-07 17:35:29 -0400

    Mixtral changed

--- LLMs/Mistral7B.py
+++ LLMs/Mistral7B.py
@@ -1,18 +1,17 @@
 from torch import nn
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline
 
 class Mistral7B(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args
-        self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1",
-                                                         device_map="auto")
-        self.model = self.model.to(device=self.args.device)
-        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+        self.pipe = pipeline("text-generation",
+                             model="mistralai/Mistral-7B-v0.1",
+                             device=args.device,
+                             device_map=[0, 1, 2, 3],
+                             max_length=150)
 
     def forward(self, prompt):
-        model_inputs = self.tokenizer([prompt], return_tensors="pt").to(device=self.args.device)
-        generated_ids = self.model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
-        return self.tokenizer.batch_decode(generated_ids)[0]
+        return self.pipe(prompt)
 
 

ecc740f05b1e1a13a5e5da6ac38a5a2108547951
AysanAghazadeh <aya34@pitt.edu>
2024-04-07 15:46:55 -0400

    mistral7B changed

--- LLMs/Mixtral7B.py
+++ LLMs/Mixtral7B.py
@@ -1,15 +1,12 @@
 from torch import nn
 from transformers import pipeline
 
+
 class Mixtral7B(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args
-
         self.pipe = pipeline("text-generation", model="mistralai/Mistral-7B-v0.1", device=args.device, max_length=150)
 
     def forward(self, prompt):
-
         return self.pipe(prompt)
-
-

f771fc2846cf84f768afb04276cdb85f9c542009
AysanAghazadeh <aya34@pitt.edu>
2024-04-07 18:58:18 -0400

    PixArt changed

--- T2I_models/PitxArt.py
+++ T2I_models/PitxArt.py
@@ -8,7 +8,7 @@ class PixArt(nn.Module):
         super(PixArt, self).__init__()
         self.pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS",
                                                         torch_dtype=torch.float16,
-                                                        device_map='auto')
+                                                        device=args.device)
 
     def forward(self, prompt):
         image = self.pipe(prompt).images[0]

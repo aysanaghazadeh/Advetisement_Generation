dbca03735f4a038f535df87a945acfa17925cf37
AysanAghazadeh <aya34@pitt.edu>
2024-04-07 15:49:17 -0400

    PixArt changed

--- T2I_models/PitxArt.py
+++ T2I_models/PitxArt.py
@@ -6,8 +6,9 @@ from diffusers import PixArtAlphaPipeline
 class PixArt(nn.Module):
     def __init__(self, args):
         super(PixArt, self).__init__()
-        self.pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", torch_dtype=torch.float16)
-        # self.pipe = self.pipe.to(device=args.device)
+        self.pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS",
+                                                        torch_dtype=torch.float16,
+                                                        device=args.device)
 
     def forward(self, prompt):
         image = self.pipe(prompt).images[0]

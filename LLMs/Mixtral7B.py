dcea395feafd2c2dfe33aa5a62557c3c123e182f
AysanAghazadeh <aya34@pitt.edu>
2024-04-07 17:34:09 -0400

    Mistral changed

--- LLMs/Mixtral7B.py
+++ LLMs/Mixtral7B.py
@@ -6,7 +6,11 @@ class Mixtral7B(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args
-        self.pipe = pipeline("text-generation", model="mistralai/Mistral-7B-v0.1", device=args.device, max_length=150)
+        self.pipe = pipeline("text-generation",
+                             model="mistralai/Mistral-7B-v0.1",
+                             device=args.device,
+                             device_map=[0, 1, 2, 3],
+                             max_length=150)
 
     def forward(self, prompt):
         return self.pipe(prompt)

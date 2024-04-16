9e63612176d7edcd642fb82ec3a7f00ef58d44f6
AysanAghazadeh <aya34@pitt.edu>
2024-04-16 19:12:51 -0400

    data parallelism

--- configs/training_config.py
+++ configs/training_config.py
@@ -119,6 +119,7 @@ def get_args():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     if torch.cuda.is_available():
         print(f"Number of GPUs available: {torch.cuda.device_count()}")
+        print(f"devices: {torch.cuda.device}")
     args.device = device
     print("Arguments are:\n", args, '\n', '-'*40)
     return args

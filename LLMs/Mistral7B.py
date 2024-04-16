d144f3bcc03f70e7a591e4ae09f7d71f51cfc924
AysanAghazadeh <aya34@pitt.edu>
2024-04-15 21:25:50 -0400

    mistral changed

--- LLMs/Mistral7B.py
+++ LLMs/Mistral7B.py
@@ -1,8 +1,24 @@
 from torch import nn
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 
 class Mistral7B(nn.Module):
+    def __init__(self, args):
+        super(Mistral7B, self).__init__()
+        self.args = args
+        self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="auto")
+        if not args.train:
+            self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+    def forward(self, inputs):
+        if not self.args.train:
+            inputs = self.tokenizer(inputs, return_tensors="pt").to(device=self.args.device)
+            generated_ids = self.model.generate(**inputs, max_new_tokens=300)
+            return self.tokenizer.batch_decode(generated_ids)[0]
+        return self.model(**inputs)
+
+
+class Mistral7BPipeline(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args

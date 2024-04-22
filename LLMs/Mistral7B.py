319294f3cec8761002c911dfc13d379fac3e9d1e
AysanAghazadeh <aya34@pitt.edu>
2024-04-22 11:12:18 -0400

    data changed

--- LLMs/Mistral7B.py
+++ LLMs/Mistral7B.py
@@ -12,7 +12,7 @@ class Mistral7B(nn.Module):
             self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1",
                                                               device_map="auto")
         else:
-            bnb_config= BitsAndBytesConfig(
+            bnb_config = BitsAndBytesConfig(
                 load_in_8bit=True,
                 # bnb_4bit_quant_type="nf4",
                 # bnb_4bit_use_double_quant=True,
@@ -21,7 +21,7 @@ class Mistral7B(nn.Module):
             self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1",
                                                               device_map='auto',
                                                               quantization_config=bnb_config)
-            self.model.gradient_checkpointing_enable()
+            self.model = self.model.gradient_checkpointing_enable()
             if torch.cuda.device_count() > 1:
                 self.model.is_parallelizable = True
                 self.model.model_parallel = True

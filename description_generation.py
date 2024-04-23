e62c36095d1a080c8c42ab7a6f31ea4cbbf829b4
AysanAghazadeh <aya34@pitt.edu>
2024-04-23 17:15:09 -0400

    error fixed

--- description_generation.py
+++ description_generation.py
@@ -29,7 +29,7 @@ def get_descriptions(args):
         image = Image.open(os.path.join(args.data_path, args.test_set_images, image_url))
         prompt = f"USER:<image>\nDescribe the image in detail.\nASSISTANT:"
         outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 512})
-        description = outputs[0]['generated_text']
+        description = outputs[0]['generated_text'].split('ASSISTANT: ')[-1]
         print(f'output of image {image_url} is {description}')
         print('-' * 40)
         pair = [image_url, description]

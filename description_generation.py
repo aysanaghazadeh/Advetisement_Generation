f1b49eaf1f8274454add5cb035597807d76fa6d1
AysanAghazadeh <aya34@pitt.edu>
2024-04-23 17:03:20 -0400

    error fixed

--- description_generation.py
+++ description_generation.py
@@ -1,4 +1,4 @@
-from transformers import AutoProcessor, LlavaForConditionalGeneration
+from transformers import AutoProcessor, LlavaForConditionalGeneration, pipeline
 from util.data.trian_test_split import get_test_data
 from PIL import Image
 import os
@@ -9,9 +9,9 @@ from configs.inference_config import get_args
 
 def get_model():
     # Load model directly
-    processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-13b-hf", device_map='auto')
-    model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-13b-hf", device_map='auto')
-    return processor, model
+    model_id = "llava-hf/llava-1.5-13b-hf"
+    pipe = pipeline("image-to-text", model=model_id, device_map='auto')
+    return pipe
 
 
 def get_descriptions(args):
@@ -24,13 +24,12 @@ def get_descriptions(args):
         writer = csv.writer(file)
         # Write the header
         writer.writerow(['ID', 'description'])
-    model, processor = get_model()
+    pipe = get_model()
     for image_url in test_images:
         image = Image.open(os.path.join(args.data_path, args.test_set_images, image_url))
         prompt = f"USER:<image>\nDescribe the image in detail.\nASSISTANT:"
-        inputs = processor(prompt, image).to(device=args.device)
-        generate_ids = model.generate(**inputs, max_new_tokens=512)
-        description = processor.decode(generate_ids[0][2:], skip_special_tokens=True)
+        outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 512})
+        description = outputs['generated_text']
         pair = [image_url, description]
         with open(description_file, 'w', newline='') as file:
             writer = csv.writer(file)

ed2eed684a2066f9bce91ecaab2aeb551c651ad9
AysanAghazadeh <aya34@pitt.edu>
2024-04-27 16:07:18 -0400

    last update

--- configs/inference_config.py
+++ configs/inference_config.py
@@ -96,7 +96,7 @@ def parse_args():
                         help='Path to the folder of prompts. Set the name of prompt files as: {text_input_type}.jinja')
     parser.add_argument('--fine_tuned',
                         type=bool,
-                        default=True,
+                        default=False,
                         help='True if you want to use the fine-tuned model')
     return parser.parse_args()
 

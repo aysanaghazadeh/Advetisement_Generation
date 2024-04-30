b9b19d9f6cad1a2cd4c57ad6993f9db3fe541b3c
AysanAghazadeh <aya34@pitt.edu>
2024-04-30 13:33:55 -0400

    evaluation changed

--- evaluate.py
+++ evaluate.py
@@ -7,7 +7,7 @@ from Evaluation.metrics import Metrics
 from configs.inference_config import get_args
 
 TOPIC_FILE = '../Data/PittAd/train/Topics_train.json'
-RESULT_FILE = '/Users/aysanaghazadeh/experiments/results/simple_llava_description_test_set.csv_PixArt_20240424_213628.csv'
+RESULT_FILE = '../experiments/results/simple_llava_description_test_set.csv_PixArt_20240424_213628.csv'
 topic_map = {'1': ["Restaurants", "cafe", "fast food"],
              '2': ["Chocolate", "cookies", "candy", "ice cream"],
              '3': ["Chips", "snacks", "nuts", "fruit", "gum", "cereal", "yogurt", "soups"],

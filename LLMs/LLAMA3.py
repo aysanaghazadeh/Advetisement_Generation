ea97dc6ae166a158cb8f113eb13a6812a1903fee
AysanAghazadeh <aya34@pitt.edu>
2024-04-30 20:24:38 -0400

    training data changed

--- LLMs/LLAMA3.py
+++ LLMs/LLAMA3.py
@@ -36,5 +36,7 @@ class LLAMA3(nn.Module):
         if not self.args.train:
             inputs = self.tokenizer(inputs, return_tensors="pt").to(device=self.args.device)
             generated_ids = self.model.generate(**inputs, max_new_tokens=200)
-            return self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+            output = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+            print(output)
+            return output
         # return self.model(**inputs)

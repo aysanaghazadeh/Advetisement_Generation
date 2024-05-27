b86298822c9db285f404675dfacbf0289e2b6bb3
AysanAghazadeh <aya34@pitt.edu>
2024-05-27 15:58:47 -0400

    device changed

--- T2I_models/PitxArt.py
+++ T2I_models/PitxArt.py
@@ -7,9 +7,9 @@ class PixArt(nn.Module):
     def __init__(self, args):
         super(PixArt, self).__init__()
         self.pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS",
-                                                        torch_dtype=torch.float16)
-        self.pipe = self.pipe.to(device=args.device)
-        # self.pipe = self.pipe.to(device=f'cuda:{1}')
+                                                        torch_dtype=torch.float16,
+                                                        device_map='auto')
+        # self.pipe = self.pipe.to(device=args.device)
     def forward(self, prompt):
         image = self.pipe(prompt).images[0]
         return image

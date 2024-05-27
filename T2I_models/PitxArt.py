04b72d4abf880455405e8fb8bb210df810fe2150
AysanAghazadeh <aya34@pitt.edu>
2024-05-27 16:09:27 -0400

    device changed

--- T2I_models/PitxArt.py
+++ T2I_models/PitxArt.py
@@ -8,10 +8,18 @@ class PixArt(nn.Module):
         super(PixArt, self).__init__()
         self.device = args.device
         self.pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS",
-                                                        torch_dtype=torch.float16,
-                                                        device_map={0: self.device})
-        # self.pipe = self.pipe.to(device=args.device)
+                                                        torch_dtype=torch.float16)
+        self.pipe = self.pipe.to(device=args.device)
+        self.pipe = nn.DataParallel(self.pipe)
+
     def forward(self, prompt):
-        prompt = prompt.to(self.device)
-        image = self.pipe(prompt).images[0]
+        # prompt = prompt.to(self.device)
+        # image = self.pipe(prompt).images[0]
+        # return image
+        if isinstance(prompt, str):
+            prompt = [prompt]
+
+            # Perform the forward pass with the pipeline
+        output = self.pipe(prompt)
+        image = output[0].images[0]
         return image

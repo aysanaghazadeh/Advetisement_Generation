ae1d926bd02eae58ca680e5bde4edce066b8a697
AysanAghazadeh <aya34@pitt.edu>
2024-05-27 16:03:47 -0400

    device changed

--- T2I_models/PitxArt.py
+++ T2I_models/PitxArt.py
@@ -6,10 +6,12 @@ from diffusers import PixArtAlphaPipeline
 class PixArt(nn.Module):
     def __init__(self, args):
         super(PixArt, self).__init__()
+        self.device = args.device
         self.pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS",
                                                         torch_dtype=torch.float16,
-                                                        device_map='auto')
+                                                        device_map={0: self.device})
         # self.pipe = self.pipe.to(device=args.device)
     def forward(self, prompt):
+        prompt = prompt.to(self.device)
         image = self.pipe(prompt).images[0]
         return image

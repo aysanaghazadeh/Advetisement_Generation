daace8ad4613f59b668abd8bf59f204c014b106e
AysanAghazadeh <aya34@pitt.edu>
2024-05-28 10:23:12 -0400

    sdxl added

--- T2I_models/SDXLFlash.py
+++ T2I_models/SDXLFlash.py
@@ -7,9 +7,9 @@ class SDXL(nn.Module):
     def __init__(self, args):
         super(SDXL, self).__init__()
         self.device = args.device
-        pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16).to(device=args.device)
+        self.pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16).to(device=args.device)
         # Ensure sampler uses "trailing" timesteps.
-        pipe.scheduler = DPMSolverSinglestepScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+        self.pipe.scheduler = DPMSolverSinglestepScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
 
     def forward(self, prompt):
         negative_prompt = "typical,(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, NSFW"

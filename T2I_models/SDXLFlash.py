246f3939655bc7725067879cf3435b8cc7108ea4
AysanAghazadeh <aya34@pitt.edu>
2024-05-28 09:44:49 -0400

    sdxl added

---
+++ T2I_models/SDXLFlash.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+from diffusers import StableDiffusionXLPipeline, DPMSolverSinglestepScheduler
+
+
+class SDXL(nn.Module):
+    def __init__(self, args):
+        super(SDXL, self).__init__()
+        self.device = args.device
+        pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16,
+                                                         variant="fp16").to(device=args.device)
+        # Ensure sampler uses "trailing" timesteps.
+        pipe.scheduler = DPMSolverSinglestepScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+
+    def forward(self, prompt):
+        negative_prompt = "typical,(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, NSFW"
+        image = self.pipe(prompt, num_inference_steps=15, guidance_scale=6, negative_prompt=negative_prompt).images[0]
+        return image
--- T2I_models/T2I_model.py
+++ T2I_models/T2I_model.py
@@ -1,12 +1,14 @@
 from torch import nn
 from T2I_models.PitxArt import PixArt
+from T2I_models.SDXLFlash import SDXL
 
 
 class T2IModel(nn.Module):
     def __init__(self, args):
         super(T2IModel, self).__init__()
         model_map = {
-            'PixArt': PixArt
+            'PixArt': PixArt,
+            'SDXL': SDXL,
         }
         self.model = model_map[args.T2I_model](args)
 

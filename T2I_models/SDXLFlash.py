7177352851ad09dc4f256f782afcd750e1230ea4
AysanAghazadeh <aya34@pitt.edu>
2024-06-05 14:40:16 -0400

    bug fixed

--- T2I_models/SDXLFlash.py
+++ T2I_models/SDXLFlash.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from diffusers import StableDiffusionXLPipeline, DPMSolverSinglestepScheduler
+from diffusers import StableDiffusionXLPipeline, DPMSolverSinglestepScheduler, StableDiffusionPipeline
 from transformers import BitsAndBytesConfig
 
 
@@ -15,23 +15,9 @@ class SDXL(nn.Module):
             self.pipe.scheduler = DPMSolverSinglestepScheduler.from_config(self.pipe.scheduler.config,
                                                                            timestep_spacing="trailing")
         else:
-            quantization_config = BitsAndBytesConfig(
-                load_in_8bit=True,
-                bnb_8bit_compute_dtype=torch.float16
-            )
-            self.pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16)
-            # Ensure sampler uses "trailing" timesteps.
-            self.pipe.scheduler = DPMSolverSinglestepScheduler.from_config(self.pipe.scheduler.config,
-                                                                           timestep_spacing="trailing")
-            self.apply_quantization(self.pipe)
-
-            # Move the model back to the appropriate device
+            model_id = "runwayml/stable-diffusion-v1-5"
+            self.pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
             self.pipe = self.pipe.to(device=args.device)
-
-    def apply_quantization(self, model):
-        model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
-        torch.quantization.prepare(model, inplace=True)
-        torch.quantization.convert(model, inplace=True)
 
     def forward(self, prompt):
         negative_prompt = "typical,(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, NSFW"

16cd87e8f6fad7be0732868412d3f828bb2dd742
AysanAghazadeh <aya34@pitt.edu>
2024-06-05 14:18:29 -0400

    bug fixed

--- T2I_models/SDXLFlash.py
+++ T2I_models/SDXLFlash.py
@@ -1,15 +1,26 @@
 import torch
 from torch import nn
 from diffusers import StableDiffusionXLPipeline, DPMSolverSinglestepScheduler
-
+from transformers import BitsAndBytesConfig
 
 class SDXL(nn.Module):
     def __init__(self, args):
         super(SDXL, self).__init__()
         self.device = args.device
-        self.pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16).to(device=args.device)
-        # Ensure sampler uses "trailing" timesteps.
-        self.pipe.scheduler = DPMSolverSinglestepScheduler.from_config(self.pipe.scheduler.config, timestep_spacing="trailing")
+        if not(args.train):
+            self.pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16).to(device=args.device)
+            # Ensure sampler uses "trailing" timesteps.
+            self.pipe.scheduler = DPMSolverSinglestepScheduler.from_config(self.pipe.scheduler.config, timestep_spacing="trailing")
+        else:
+            quantization_config = BitsAndBytesConfig(
+                load_in_8bit=True,
+                bnb_8bit_compute_dtype=torch.float16
+            )
+            self.pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16,
+                                                                  model_kwargs={"quantization_config": quantization_config})
+            # Ensure sampler uses "trailing" timesteps.
+            self.pipe.scheduler = DPMSolverSinglestepScheduler.from_config(self.pipe.scheduler.config, timestep_spacing="trailing")
+
 
     def forward(self, prompt):
         negative_prompt = "typical,(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, NSFW"

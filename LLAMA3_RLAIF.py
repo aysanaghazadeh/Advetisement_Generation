28251ca74fd0a60fa332b969e97657a2d0cfce5c
AysanAghazadeh <aya34@pitt.edu>
2024-06-09 18:58:23 -0400

    persuasiveness result

--- LLAMA3_RLAIF.py
+++ LLAMA3_RLAIF.py
@@ -87,7 +87,8 @@ def train(args):
     config = PPOConfig(
         model_name="meta-llama/Meta-Llama-3-8B",
         learning_rate=1.41e-5,
-        batch_size=1
+        batch_size=1,
+        mini_batch_size=1
     )
     model, tokenizer = get_model()
     optimizer = Adam(model.parameters(), lr=args.lr)

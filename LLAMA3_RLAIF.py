947da258e58d181d9acdc69f83a73aa1e3dcfd62
AysanAghazadeh <aya34@pitt.edu>
2024-06-09 22:44:44 -0400

    persuasiveness result

--- LLAMA3_RLAIF.py
+++ LLAMA3_RLAIF.py
@@ -69,7 +69,7 @@ def get_model():
         load_in_4bit=True
     )
     model = get_peft_model(model, lora_config)
-    print(f'model\'s trainable parameters: {model.print_trainable_parameters()}')
+    # print(f'model\'s trainable parameters: {model.print_trainable_parameters()}')
     if torch.cuda.device_count() > 1:
         print(f'torch cuda count: {torch.cuda.device_count()}')
         model.is_parallelizable = True

cf90fb6a630fc7ac7665d5e11708210eff3ad6fd
AysanAghazadeh <aya34@pitt.edu>
2024-06-09 21:32:35 -0400

    persuasiveness result

--- LLAMA3_RLAIF.py
+++ LLAMA3_RLAIF.py
@@ -112,7 +112,7 @@ def train(args):
         mini_batch_size=1
     )
     model, tokenizer = get_model()
-    model = model.to(args.device)
+    model = model.to(device='cuda:2')
     reward_model = RewardModel(args)
     dataset = get_LLAMA3_RLAIF_Dataloader(args)
     ppo_trainer = PPOTrainer(

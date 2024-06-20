517c724cdc5e0cefed3fc902ebd9a729f901b01b
AysanAghazadeh <aya34@pitt.edu>
2024-06-19 22:28:55 -0400

    llama evaluation

--- util/data/data_util.py
+++ util/data/data_util.py
@@ -169,7 +169,7 @@ def get_LLAMA3_RLAIF_training_data(args, image_urls):
     QAs = json.load(open(os.path.join(args.data_path, args.test_set_QA)))
     dataset = {'query': [], 'action_reason': []}
     for image_url in image_urls:
-        QA = str(QAs[image_url][0])
+        QA = QAs[image_url][0]
         dataset['query'].append(QA)
         dataset['action_reason'].append(QA)
 

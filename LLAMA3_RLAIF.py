c4c5ba45a2a1a706c6d012fef970b1acad152e8d
AysanAghazadeh <aya34@pitt.edu>
2024-06-20 15:12:16 -0400

    llama evaluation

--- LLAMA3_RLAIF.py
+++ LLAMA3_RLAIF.py
@@ -91,7 +91,7 @@ def train(args):
         for batch in tqdm(ppo_trainer.dataloader):
             batch["input_ids"] = [tokenizer.encode(batch['query']['query'][i]) for i in range(len(batch['query']['query']))]
             query_tensors = batch["input_ids"]
-            query_tensors = [[torch.stack([torch.tensor(tensor) for tensor in query_tensor])] for query_tensor in query_tensors]
+            query_tensors = [torch.stack([torch.stack([torch.tensor(tensor) for tensor in query_tensor])]) for query_tensor in query_tensors]
             response_tensors = [ppo_trainer.generate(query_tensor, **generation_kwargs) for query_tensor in query_tensors]
             batch["response"] = [[tokenizer.decode(r.squeeze()) for r in response_tensor] for response_tensor in response_tensors]
             texts = [r for q, r in zip(batch["query"], batch["response"])]

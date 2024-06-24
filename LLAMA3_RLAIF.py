d0267603d4b3bcd5579ae6ae9c2a7702ba5f0eb4
AysanAghazadeh <aya34@pitt.edu>
2024-06-23 21:16:36 -0400

    PPO training with batch-size bigger than 1

--- LLAMA3_RLAIF.py
+++ LLAMA3_RLAIF.py
@@ -96,7 +96,7 @@ def train(args):
         "min_length": 1,
         "top_k": 0.0,
         "top_p": 1.0,
-        "max_new_tokens": 125,
+        "max_new_tokens": 50,
         "do_sample": True,
         "pad_token_id": tokenizer.eos_token_id,
     }

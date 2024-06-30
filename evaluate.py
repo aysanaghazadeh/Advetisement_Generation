b8c4603921392c94ebb0d8858ecef8100821585d
AysanAghazadeh <aya34@pitt.edu>
2024-06-30 17:19:12 -0400

    whoops added

--- evaluate.py
+++ evaluate.py
@@ -313,14 +313,14 @@ class Evaluation:
         results = {'acc@1': 0}
         fieldnames = ['id', 'acc@1']
         # csv_file_path = os.path.join(args.result_path, ''.join(['action_reason_llava_', args.description_file]))
-        csv_file_path = os.path.join(args.result_path, 'whoops_llava_without_description.csv')
+        csv_file_path = os.path.join(args.result_path, 'whoops_commonsense_llava.csv')
         with open(csv_file_path, 'w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
         # dataset = load_dataset('nlphuji/whoops',
         #                        cache_dir=None,
         #                        use_auth_token='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')
-        QA_file = os.path.join(args.data_path, 'train/whoops_caption.json')
+        QA_file = os.path.join(args.data_path, 'train/whoops_commonsense_category.json')
         QAs = json.load(open(QA_file))
 
         # QAs = {}

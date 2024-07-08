992062d9f42ea56aff4b29b2ffbb24755aadcbff
AysanAghazadeh <aya34@pitt.edu>
2024-07-07 23:42:53 -0400

    random symbolic added

--- evaluate.py
+++ evaluate.py
@@ -412,7 +412,7 @@ class Evaluation:
         QAs = json.load(open(QA_file))
         pipe = LLM(args)
         descriptions = pd.read_csv(os.path.join(args.data_path, 'train',
-                                                f'simple_llava_description.csv'))
+                                                f'combine_llava_description.csv'))
         print(descriptions)
         for image_url in QAs:
             description = descriptions.loc[descriptions['ID'] == image_url]['description'].values

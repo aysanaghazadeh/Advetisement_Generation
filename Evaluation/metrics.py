3f7ac4bf2a4a84ce73c98a9c22de76bc6c8b5808
AysanAghazadeh <aya34@pitt.edu>
2024-07-09 18:41:34 -0400

    random symbolic added

--- Evaluation/metrics.py
+++ Evaluation/metrics.py
@@ -506,7 +506,7 @@ class Whoops:
         )
         model_id_map = {
             'LLAVA': "llava-hf/llava-1.5-13b-hf",
-            'LLAVA_LLAMA': "xtuner/llava-llama-3-8b",
+            'LLAVA_phi': "xtuner/llava-phi-3-mini-hf",
             'VILA': "Efficient-Large-Model/VILA1.5-13b",
             'InternVL': "OpenGVLab/InternVL-Chat-V1-5"
         }

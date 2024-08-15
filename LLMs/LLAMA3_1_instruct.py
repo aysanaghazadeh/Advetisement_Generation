8ad38ee5199ec4bd58e940c13a574a176749d08c
AysanAghazadeh <aya34@pitt.edu>
2024-08-15 16:23:14 -0400

    llama3.1 instruct added;

---
+++ LLMs/LLAMA3_1_instruct.py
@@ -0,0 +1,40 @@
+from torch import nn
+from transformers import BitsAndBytesConfig, pipeline
+import torch
+from peft import PeftModel
+import os
+
+
+class LLAMA31(nn.Module):
+    def __init__(self, args):
+        super(LLAMA31, self).__init__()
+        self.args = args
+        if not args.train:
+            quantization_config = BitsAndBytesConfig(
+                load_in_8bit=True,
+                bnb_8bit_compute_dtype=torch.float16
+            )
+            model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+
+            self.pipeline = pipeline(
+                "text-generation",
+                model=model_id,
+                model_kwargs={"torch_dtype": torch.bfloat16},
+                device_map="auto",
+            )
+
+
+    def forward(self, prompt):
+        if not self.args.train:
+            messages = [
+                {"role": "user", "content": prompt},
+            ]
+            output = self.pipeline(messages, max_new_tokens=256)[0]["generated_text"][-1]
+            output = output.replace('</s>', '')
+            output = output.replace("['", '')
+            output = output.replace("']", '')
+            output = output.replace('["', '')
+            output = output.replace('"]', '')
+            output = output.split(':')[-1]
+            return output
+        # return self.model(**inputs)
--- LLMs/LLM.py
+++ LLMs/LLM.py
@@ -4,7 +4,7 @@ from LLMs.LLAMA3 import LLAMA3
 from LLMs.phi import Phi
 from LLMs.Mistral7BInstruct import Mistral7BInstruct
 from LLMs.vicuna import Vicuna
-
+from LLMs.LLAMA3_1_instruct import LLAMA31
 
 class LLM(nn.Module):
     def __init__(self, args):
@@ -12,6 +12,7 @@ class LLM(nn.Module):
         model_map = {
             'Mistral7B': Mistral7B,
             'LLAMA3': LLAMA3,
+            'LLAMA3.1': LLAMA31,
             'phi': Phi,
             'Mistral7BInstruct': Mistral7BInstruct,
             'vicuna': Vicuna

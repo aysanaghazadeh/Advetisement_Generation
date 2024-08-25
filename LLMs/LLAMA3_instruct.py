5bee8397e669bb44b594c7cad224aa0cdd2c8bd5
AysanAghazadeh <aya34@pitt.edu>
2024-08-25 12:46:06 -0400

    fine-tuned model added

--- LLMs/LLAMA3_instruct.py
+++ LLMs/LLAMA3_instruct.py
@@ -1,5 +1,5 @@
 from torch import nn
-from transformers import BitsAndBytesConfig, pipeline
+from transformers import BitsAndBytesConfig, pipeline, AutoModelForCausalLM, AutoTokenizer
 import torch
 from peft import PeftModel
 import os
@@ -14,18 +14,30 @@ class LLAMA3Instruct(nn.Module):
                 load_in_8bit=True,
                 bnb_8bit_compute_dtype=torch.float16
             )
-            model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+            if args.fine_tuned:
+                self.model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-instruct",
+                                                             token='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',
+                                                             device_map='auto')
+                self.tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-instruct",
+                                                          token='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')
+                self.tokenizer.pad_token = self.tokenizer.eos_token
+                self.tokenizer.padding_side = "right"
+                self.model = PeftModel.from_pretrained(self.model,
+                                                       os.path.join(args.model_path,
+                                                                    'my_LLAMA3_instruct_large_sample_OnInternVL_model/checkpoint-2300/'))
+            else:
+                model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
-            self.pipeline = pipeline(
-                "text-generation",
-                model=model_id,
-                token='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',
-                model_kwargs={"torch_dtype": torch.bfloat16},
-                device_map="auto",
-            )
+                self.pipeline = pipeline(
+                    "text-generation",
+                    model=model_id,
+                    token='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',
+                    model_kwargs={"torch_dtype": torch.bfloat16},
+                    device_map="auto",
+                )
 
     def forward(self, prompt):
-        if not self.args.train:
+        if not self.args.fine_tuned:
             messages = [
                 {"role": "system", "content": "Be a helpful assistant"},
                 {"role": "user", "content": prompt},

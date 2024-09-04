1b239303fbdb696c3d84e165b0bd0d84c4389426
AysanAghazadeh <aya34@pitt.edu>
2024-09-04 14:32:27 -0400

    model changed

--- VLMs/InternVL2.py
+++ VLMs/InternVL2.py
@@ -114,6 +114,7 @@ class InternVL(nn.Module):
         # print(f'User: {prompt}')
         # print(f'Assistant: {response}')
         # print('*' * 10)
+        path = "OpenGVLab/InternVL-Chat-V1-1"
         image_processor = CLIPImageProcessor.from_pretrained(path)
         image = image.resize((448, 448))
         pixel_values = image_processor(images=image, return_tensors='pt').pixel_values.to(torch.bfloat16).cuda()

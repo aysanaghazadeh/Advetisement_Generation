02f6b172c73607c4fcf4722925fafa13c03bd794
AysanAghazadeh <aya34@pitt.edu>
2024-09-29 13:14:59 -0400

    script for text_image alignment added

---
+++ scripts/text_image_alignment_evaluation.sh
@@ -0,0 +1,1 @@
+python evaluate.py --config_type=DEFAULT --evaluation_type=text_image_alignment --description_file=../experiments/results/PSA_original_images_description.csv --LLM=LLAMA3_instruct --fine_tuned=True

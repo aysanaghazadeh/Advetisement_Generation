1769b2c72343c54467f172fc8f564c779fc74ce9
AysanAghazadeh <aya34@pitt.edu>
2024-09-28 23:47:05 -0400

    description generation script added

---
+++ scripts/description_generation.sh
@@ -0,0 +1,7 @@
+python description_generation.py
+        --config_type=DEFAULT
+        --VLM=InternVL
+        --description_type=IN
+        --test_set_QA=../experiments/results/IN_InternVL_LLM_input_LLAMA3_instruct_FTFalse_PSA_AuraFlow_20240925_112154_description.csv
+        --test_set_images=train_images_all
+        --VLM_prompt=IN_description_single_paragraph.jinja

7cad9eada9426ce29fa8dfca565379f8708474de
AysanAghazadeh <aya34@pitt.edu>
2024-10-29 17:49:23 -0400

    VQA script added

--- evaluate.py
+++ evaluate.py
@@ -964,7 +964,7 @@ class Evaluation:
     @staticmethod
     def evaluate_VQA_score(args):
         results = pd.read_csv(os.path.join(args.result_path, args.result_file)).values
-        QA = json.load(open(os.path.join(args.data_path, 'QA_Combined_Action_Reason_train.json')))
+        QA = json.load(open(os.path.join(args.data_path, 'train/QA_Combined_Action_Reason_train.json')))
         saving_path = os.path.join(args.result_path, args.result_file).replace('.csv', '_VQA_scores.json')
         VQA_scores = {}
         clip_flant5_score = t2v_metrics.VQAScore(model='clip-flant5-xxl')

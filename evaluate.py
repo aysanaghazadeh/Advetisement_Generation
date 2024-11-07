c855da5628d9d4d1394e5abab6cf279b85878d52
AysanAghazadeh <aya34@pitt.edu>
2024-11-07 01:36:36 -0500

    changed

--- evaluate.py
+++ evaluate.py
@@ -114,18 +114,18 @@ class Evaluation:
     @staticmethod
     def evaluate_llm_multi_question_persuasiveness_ranking(args):
         score_metrics = Metrics(args)
-        saving_path = os.path.join(args.result_path, f'AR_Auraflow_{args.VLM}_llm_multi_question_persuasiveness_psa.json')
+        saving_path = os.path.join(args.result_path, f'AR_Auraflow_{args.VLM}_llm_multi_question_persuasiveness_com.json')
         print(saving_path)
         print(args.result_path)
         print(args.result_file)
         results2 = pd.read_csv(os.path.join(args.result_path,
-                                            'LLAMA3Instruct_descriptions_AuraFlow_20240817_185858.csv'))
+                                            'IN_InternVL_AR_AuraFlow_20240816_214421_description_single_paragraph_full_description.csv'))
         results1 = pd.read_csv(os.path.join(args.result_path,
-                                            'AR_SDXL_20240613_204248.csv'))
+                                            'IN_InternVL_AR_AuraFlow_20240816_214421_description_single_paragraph_full_description.csv'))
         descriptions2 = pd.read_csv(os.path.join(args.result_path,
-                                                 'IN_InternVL_AR_AuraFlow_20240924_210335_description_single_paragraph_full_description.csv'))
+                                                 'IN_InternVL_AR_AuraFlow_20240816_214421_description_single_paragraph_full_description.csv'))
         descriptions1 = pd.read_csv(os.path.join(args.result_path,
-                                                 'IN_InternVL_AR_AuraFlow_20240924_210335_description_single_paragraph_full_description.csv'))
+                                                 'IN_InternVL_AR_AuraFlow_20240816_214421_description_single_paragraph_full_description.csv'))
         persuasiveness_scores = {}
         for row in descriptions1.values:
             image_url = row[0]
